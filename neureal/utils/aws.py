d1dc6219470901e5915c80f85062db98b75c09af
picarro-cnelson <cnelson@picarro.com>
2023-04-01 18:27:11 -0700

    images folder

--- neureal/utils/aws.py
+++ neureal/utils/aws.py
@@ -14,7 +14,7 @@ def get_bucket_images():
         object_key = object['Key']
         if object_key.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
             response = s3.get_object(Bucket=bucket_name, Key=object_key)
-            file_name = os.path.join('static/images_2', object_key)
+            file_name = os.path.join('static/images', object_key)
             os.makedirs(os.path.dirname(file_name), exist_ok=True)
             with open(file_name, 'wb') as f:
                 f.write(response['Body'].read())
