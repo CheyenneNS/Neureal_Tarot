6eab1aac58d8af64c5e0f31c86ed721a13867401
picarro-cnelson <cnelson@picarro.com>
2023-04-01 18:31:01 -0700

    aws util to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from jinja2 import Environment, FileSystemLoader
 from neureal.routers import draw_router
+from neureal.utils import aws
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="neureal/static"), name="static")
@@ -13,6 +14,7 @@ env = Environment(loader=FileSystemLoader('neureal/templates'))
 # Define a route that renders a template
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    aws.get_bucket_images()
     template = env.get_template('index.html')
     content = template.render(title="Neureal Tarot")
     return HTMLResponse(content=content)
